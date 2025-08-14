292c6171c907722e0850f457b781a619fbf6bf7b
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-08-14 16:31:19 -0600

    TodoRepository

--- src/main/java/com/example/todolistapi/repository/TodoRepository.java
+++ src/main/java/com/example/todolistapi/repository/TodoRepository.java
@@ -1,4 +1,12 @@
 package com.example.todolistapi.repository;
 
-public class TodoRepository {
+import com.example.todolistapi.model.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TodoRepository extends JpaRepository <Todo, Long> {
+    Optional <Todo> findByIdAndUser (Long id, User user);
 }
