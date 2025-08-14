a6591a11bcaf20cac36aef78a76aa4d21bb69411
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-08-14 16:22:15 -0600

    Todo entity

--- src/main/java/com/example/todolistapi/model/Todo.java
+++ src/main/java/com/example/todolistapi/model/Todo.java
@@ -1,4 +1,22 @@
 package com.example.todolistapi.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
