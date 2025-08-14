bb70a64d68990a229fb724349640a0c172ee7120
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-08-14 16:26:10 -0600

    User entity

--- src/main/java/com/example/todolistapi/model/User.java
+++ src/main/java/com/example/todolistapi/model/User.java
@@ -1,4 +1,26 @@
 package com.example.todolistapi.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List <Todo> todos;
 }
